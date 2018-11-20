355870e01e9dbfdf5511917c98aff4a4e888a36c
JorgeLearte <34438246+JorgeLearte@users.noreply.github.com>
2018-11-20 14:49:16 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,51 +1,42 @@
-# Reading training and test tables:
-
+# 1. Reading training and test tables:
 subject_t <- read.table("./test/subject_test.txt")
 subject_train <- read.table("./train/subject_train.txt")
 
-X_test <- read.table("./test/X_test.txt")
-X_train <- read.table("./train/X_train.txt")
-y_test <- read.table("./test/y_test.txt")
-y_train <- read.table("./train/y_train.txt")
-activity_labels = read.table("./activity_labels.txt")
-features <- read.table("./features.txt")  
+xtest <- read.table("./test/xtest.txt")
+xtrain <- read.table("./train/xtrain.txt")
+ytest <- read.table("./test/ytest.txt")
+ytrain <- read.table("./train/ytrain.txt")
+activityLabels = read.table("./activityLabels.txt")
+feat <- read.table("./feat.txt")  
 
-colnames(X_train) <- features[,2] 
-colnames(y_train) <-"activityId"
-colnames(subject_train) <- "subjectId"
+colnames(xtrain) <- feat[,2] 
+colnames(ytrain) <-"aId"
+colnames(subject_train) <- "sId"
 
-colnames(X_test) <- features[,2] 
-colnames(y_test) <- "activityId"
-colnames(subject_test) <- "subjectId"
+colnames(xtest) <- feat[,2] 
+colnames(ytest) <- "aId"
+colnames(subject_test) <- "sId"
 
-colnames(activity_labels) <- c('activityId','activityType')
+colnames(activityLabels) <- c('aId','activityType')
 
-mergedtrain <- cbind(y_train, subject_train, X_train)
-mergedtest <- cbind(y_test, subject_test, X_test)
-mergeddata <- rbind(mergedtrain, mergedtest)
+mergedTrain <- cbind(ytrain, subject_train, xtrain)
+mergedTest <- cbind(ytest, subject_test, xtest)
+mergedData <- rbind(mergedtrain, mergedTest)
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-colNames <- colnames(mergeddata)
-
-mean_and_std <- (grepl("activityId" , colNames) | 
-                   grepl("subjectId" , colNames) | 
+# 2. Extracts mean and standard deviation for each measurement.
+colNames <- colnames(mergedData)
+meanstd <- (grepl("aId" , colNames) | 
+                   grepl("sId" , colNames) | 
                    grepl("mean.." , colNames) | 
                    grepl("std.." , colNames) 
 )
-
-
-setForMeanAndStd <- mergeddata[ , mean_and_std == TRUE]
-
-
-# 3. Uses descriptive activity names to name the activities in the data set
-
-setWithActivityNames <- merge(setForMeanAndStd, activity_labels,
-                              by='activityId',
+setMeanSt <- mergedData[ , meanstd == TRUE]
+# 3. Set activity names
+setWithActivityNames <- merge(setMeanSt, activityLabels,
+                              by='aId',
                               all.x=TRUE)
-
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-tidydata <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
-tidydata <- tidydata[order(tidydata$subjectId, secTidySet$activityId),]
+# 5. Create tidy data set with the mean of each variable for each case
+tidydata <- aggregate(. ~sId + aId, setWithActivityNames, mean)
+tidydata <- tidydata[order(tidydata$sId, secTidySet$aId),]
 write.table(tidydata, "tidydata.txt", row.name=FALSE)
-
 
